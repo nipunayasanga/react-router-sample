734dc8e3868e23c17499225cf74c2c454878c5d9
nipunayasanga <nipunayes@gmail.com>
2024-08-13 23:35:52 +0530

    update

--- testproject/src/components/pages/AboutUs.jsx
+++ testproject/src/components/pages/AboutUs.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HeaderContent from '../HeaderContent/HeaderContent'
 import BodyContent from '../BodyContent/BodyContent'
 
 export default function AboutUs() {
+
+  const [username, setUserName] = useState("Guest");
+
+
   return (
     
       <>
@@ -10,11 +14,19 @@ export default function AboutUs() {
         <HeaderContent />
         <BodyContent>
           <h3>About Us Page</h3>
-          <p1>About Us Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque quas adipisci quia debitis? Harum quibusdam, hic ipsum sunt cumque tenetur quisquam aperiam sapiente aut placeat magni provident nam totam voluptatem?</p1>
+          <p1>About Us Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Eaque quas adipisci quia debitis? Harum quibusdam, hic ipsum sunt cumque tenetur 
+            quisquam aperiam sapiente aut placeat magni provident nam totam voluptatem?</p1>
         </BodyContent>
         
       </div>
     
+      <h2>Parent</h2>
+
+      <p>Welcome {username}</p>
+
+      <input type="text" placeholder='Enter Your Username' onChange={(e) => {setUserName(e.target.value)}} />
+
      </>
       
     
